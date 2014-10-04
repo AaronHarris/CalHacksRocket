856b6a38d4032eef5959108ee371a919b221a407
unknown <steven.edouard1@gmail.com>
2014-10-04 02:11:14 +0000

    Take out access otken

--- routes/fire.js
+++ routes/fire.js
@@ -6,8 +6,8 @@ router.post('/', function(req, res) {
   
   //fire the rocket!
   //TODO: NEVER place your actual spark core access_token in your file
-  unirest.post('https://api.spark.io/v1/devices/53ff6e066667574833512467/cycleRelay')
-  .send('access_token=a60207c84d396e58510aae5b91f5bbfb0dccb9a7')
+  unirest.post('https://api.spark.io/v1/devices/<SPARK_CORE_ID>/cycleRelay')
+  .send('access_token=<SPARK CORE ACCESS_TOKEN>')
   .send('params=r1,1500')
   .end(function(response){
     
